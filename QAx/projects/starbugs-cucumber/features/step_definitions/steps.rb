269da0f5b7cf776f898edad07c9effe9b174507f
milani <milanisingrid@gmail.com>
2024-07-01 16:20:39 -0300

    first commit

---
+++ QAx/projects/starbugs-cucumber/features/step_definitions/steps.rb
@@ -0,0 +1,8 @@
+Quando('acesso a página principal da Starbugs') do
+  visit 'https://starbugs.vercel.app/'
+end
+
+Então('eu devo ver uma lista de cafés disponíveis') do
+  products = all ('.coffee-item')
+  expect(products.size).to be > 0
+end
